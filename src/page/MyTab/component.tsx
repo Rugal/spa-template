8af05cdb605b0aa41101588966b1cf854ad078f8
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2023-09-15 11:37:21 +0800

    Add NavigationBar

--- src/page/MyTab/component.tsx
+++ src/page/MyTab/component.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Container from "@mui/material/Container";
-import Box from '@mui/material/Box';
+import Box from "@mui/material/Box";
 import LogGroup from "../../component/LogGroup";
+import NavigationBar from "../../component/NavigationBar";
 
 const MyTab: React.FC = () =>
-  <Container maxWidth="lg">
-    <Box sx={{ my: 4 }}>
-      <LogGroup />
-    </Box>
-  </Container>;
+  <>
+    <NavigationBar />
+    <Container maxWidth="lg">
+      <Box sx={{ my: 4 }}>
+        <LogGroup />
+      </Box>
+    </Container>
+  </>;
 
 export default MyTab;
